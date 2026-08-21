90e62ed546c3658ced8579fe93fdf78c9b28e037
Morlay <morlay.null@gmail.com>
2026-08-21 17:25:46 +0800

    feat: added flash vision

--- rustpkg/playpen-config/src/preset/model.rs
+++ rustpkg/playpen-config/src/preset/model.rs
@@ -57,6 +57,24 @@ fn deepseek_provider() -> ModelProvider {
                 },
             },
             Model {
+                name: "deepseek-v4-flash-vision-exp".into(),
+                display_name: Some("DeepSeek V4 Flash Vision Exp".into()),
+                reasoning_efforts: vec![
+                    ThinkingLevel::Off,
+                    ThinkingLevel::High,
+                    ThinkingLevel::Max,
+                ],
+                input_types: vec![InputType::Text, InputType::Image],
+                context_window: 1_000_000,
+                max_tokens: 384_000,
+                cost: Cost {
+                    input: 1.0,
+                    output: 2.0,
+                    cache_read: 0.02,
+                    currency: Currency::CNY,
+                },
+            },
+            Model {
                 name: "deepseek-v4-pro".into(),
                 display_name: Some("DeepSeek V4 Pro".into()),
                 reasoning_efforts: vec![
